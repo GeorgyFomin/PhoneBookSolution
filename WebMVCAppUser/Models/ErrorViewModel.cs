44286ad1a04580f74a77ac500079d22acdc9f124
Georgy <ajvbyud@outlook.com>
2022-07-24 21:03:13 +0300

    Last Version

--- WebMVCAppUser/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace WebMVCAppUser.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
