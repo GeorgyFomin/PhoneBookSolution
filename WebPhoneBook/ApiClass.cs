44fefee1603010fa847053be9324f1b62499dc51
Georgy <ajvbyud@outlook.com>
2022-07-31 14:31:55 +0300

    ApiClient 2

--- WebPhoneBook/ApiClass.cs
+++ WebPhoneBook/ApiClass.cs
@@ -5,12 +5,7 @@ namespace WebPhoneBook
     public class ApiClient
     {
         private const string baseAddress = "https://localhost:7252/";//Или http://localhost:5252///
-        public static RestClient Rest { get; }
-        public static HttpClient Http { get; }
-        static ApiClient()
-        {
-            Rest = new(baseAddress);
-            Http = new() { BaseAddress = new Uri(baseAddress) };
-        }
+        public static RestClient Rest { get; } = new(baseAddress);
+        public static HttpClient Http { get; } = new() { BaseAddress = new Uri(baseAddress) };
     }
 }
